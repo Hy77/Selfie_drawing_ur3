a6f5b1b3d9180191e1621259a37bbb3d9a593065
Hy77 <74601064+Hy77@users.noreply.github.com>
2024-04-15 20:37:19 +1000

    Update selfie_drawing.py

--- selfie_drawing.py
+++ selfie_drawing.py
@@ -1,7 +1,7 @@
 from img_processing import ImgProcessor
 from toolpathing import PathPlanner
 from paper_detector import PaperDetector
-from ur3_move import UR3_Control
+from ur3_control_actionlib import UR3Control
 import rospy
 from sensor_msgs.msg import Image
 from message_filters import ApproximateTimeSynchronizer, Subscriber
@@ -12,7 +12,7 @@ class SelfieDrawer:
         self.img_processor = ImgProcessor()
         self.path_planner = PathPlanner
         self.paper_detector = PaperDetector()
-        # self.ur3_controller = UR3_Control()
+        self.ur3_controller = UR3Control()
         self.paper_local_info = None
         self.paper_global_info = None
         self.image = image
@@ -28,30 +28,30 @@ class SelfieDrawer:
         self.final_contours = self.img_processor.final_contours
         self.final_image = self.img_processor.final_image
 
-    def define_paper_global_coord(self):
-        self.paper_global_info = self.paper_detector.paper_info
-
     def tsp_algo(self):
         self.final_contour_updator()  # update final_contours & image
         self.path_planner = PathPlanner(self.final_contours, self.final_image)
         self.path_planner.visualization()
 
     def start_drawing(self):
-        print("--------------------- Start Paper Detecting ----------------------")
+        print("---------------------- Moving to Init Pose -----------------------")
+        # self.ur3_controller.run([0.3, -0.1, 0.47])  # init pose -> facing down to the paper
+        print("Done\n--------------------- Start Paper Detecting ----------------------")
         # self.paper_local_info = self.paper_detector.paper_info  # update paper local info
         # self.paper_global_info = self.ur3_controller.define_paper_global_coord(self.paper_local_info)  # convert to glb
-        print("---------------------- Start Paper Locating ----------------------")
+        print("Done\n---------------------- Start Paper Locating ----------------------")
         # self.ur3_controller.run(self.paper_global_info)  # let ur3 to reach 4 corners of the paper
-        print("--------------------- Start Image Processing ---------------------")
+        print("Done\n--------------------- Start Image Processing ---------------------")
         self.handle_img_processing()  # run img processing & get contours
-        print("------------------------Start TSP Planning -----------------------")
-        self.tsp_algo()  # run tsp algo get effective path
+        print("Done\n------------------------Start TSP Planning -----------------------")
+        # self.tsp_algo()  # run tsp algo get effective path
         # TODO: control ur3 to move to these coordinates
 
     def callback(self, msg_color, msg_depth):
         try:
             self.paper_detector.callback(msg_color, msg_depth)
-            self.paper_detector.paper_detection()
+            for i in range(3, 0, -1):  # 3s to make sure its stable
+                self.paper_detector.paper_detection()
             if self.paper_detector.paper_found:
                 rospy.signal_shutdown('Paper found.')
                 self.start_drawing()
